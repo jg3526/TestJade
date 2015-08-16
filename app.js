3a259a1a833766f58731103d886d0699221dd69c
Jie Gao <jg3526@users.noreply.github.com>
2015-08-16 13:08:53 -0400

    tried out reading from mongodb userlist, and separate files for routing

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var jade = require('jade');
+var stylus = require('stylus');
+var nib = require('nib');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/jadeapp');
 
 var port = 3000;
 
@@ -7,20 +15,40 @@ var port = 3000;
 var app = express();
 console.log('Express initialized!');
 
+function compile(str, path) {
+	return stylus(str)
+	.set('filename', path)
+	.use(nib())
+}
+
 // set up views folder
 app.set('views', __dirname + '/views');
 // init jade
 app.set('view engine', 'jade');
 console.log('Jade initilaized!');
 
+// Stylus Middleware
+// app.use(express.logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.methodOverride());
+// app.use(express.cookieParser('secret'));
+// app.use(express.session());
+// app.use(app.router);
+app.use(stylus.middleware(
+	{
+		src:__dirname + '/public',
+		compile: compile
+	}
+));
+
 // set static folder
 app.use(express.static(__dirname + '/public'));
 
 // render index
-app.get('/', function(req, res){
-	res.render('index',
-				{title:'Welcome'});
-});
+app.get('/', routes.index);
+app.get('/userlist', routes.userlist(db));
+// app.post('/adduser', routes.adduser(db));
 
 // listen on the port
 app.listen(port);
